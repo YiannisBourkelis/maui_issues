78231f66ce4e5d987dbf08e0f7eaa1d4ac541d1c
Yiannis Bourkelis <yiannis1234@gmail.com>
2022-11-30 01:01:52 +0200

    push modal on startup issue

---
+++ src/MauiPushModalAsyncIssue/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiPushModalAsyncIssue;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+        MainPage = new NavigationPage(new MainPage());
+
+        Application.Current.MainPage.Navigation.PushModalAsync(new NavigationPage(new LoginPage()));
+
+        Console.WriteLine($"ModalStack count: {Application.Current.MainPage.Navigation.ModalStack.Count}");
+
+    }
+}
+
---
+++ src/MauiPushModalAsyncIssue/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiPushModalAsyncIssue;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
