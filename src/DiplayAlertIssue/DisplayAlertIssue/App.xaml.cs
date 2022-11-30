46fbadf73c06409eaf5d76593b92cbb57d318462
Yiannis Bourkelis <yiannis1234@gmail.com>
2022-11-30 18:54:53 +0200

    Update App.xaml.cs

--- src/DiplayAlertIssue/DisplayAlertIssue/App.xaml.cs
+++ src/DiplayAlertIssue/DisplayAlertIssue/App.xaml.cs
@@ -13,22 +13,7 @@ public partial class App : Application
     {
         base.OnStart();
 
-        string error_issue = """
-                                        System.TypeInitializationException: The type initializer for 'Microsoft.AppCenter.Utils.ApplicationLifecycleHelperDesktop' threw an exception.
-                     ---> System.IO.FileNotFoundException: Could not load file or assembly 'System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089' or one of its dependencies.
-                    File name: 'System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089'
-                       --- End of inner exception stack trace ---
-                       at Microsoft.AppCenter.ApplicationLifecycleHelper.get_Instance()
-                       at Microsoft.AppCenter.AppCenter..ctor()
-                       at Microsoft.AppCenter.AppCenter.get_Instance()
-                       at Microsoft.AppCenter.AppCenter.PlatformStart(String appSecret, Type[] services)
-                       at Microsoft.AppCenter.AppCenter.Start(String appSecret, Type[] services)
-                       at Invoices.Startup.AppStateMachine.ProccessState() in /Users/yiannis/Projects/Invoices-App-MAUI/src/Startup/AppStateMachine.cs:line 52
-                       at Invoices.App.OnStart() in /Users/yiannis/Projects/Invoices-App-MAUI/src/Startup/App.xaml.cs:line 29
-                    """;
-
-
-        string message2 = """
+        string message = """
                     System.TypeInitializationException: The type initializer for 'Microsoft.AppCenter.Utils.ApplicationLifecycleHelperDesktop' threw an exception.
                      ---> System.IO.FileNotFoundException: Could not load file or assembly 'System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089' or one of its dependencies.
                     File name: 'System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089'
@@ -36,7 +21,9 @@ public partial class App : Application
                        at Microsoft.AppCenter.ApplicationLifecycleHelper.get_Instance()
                     """;
 
-        Application.Current.MainPage.DisplayAlert("Alert Title", message2, "OK");
+        //Application.Current.MainPage.DisplayAlert("Alert Title", message, "OK");
+
+        Application.Current.MainPage.DisplayAlert("Alert Title", "This works just fine!", "OK");
 
     }
 }
