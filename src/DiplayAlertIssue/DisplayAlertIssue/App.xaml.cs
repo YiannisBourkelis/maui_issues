e56e7612e043f2dfcfdd9e1af3d35d1e386c573e
Yiannis Bourkelis <yiannis1234@gmail.com>
2022-12-01 12:53:54 +0200

    moved code to button click

--- src/DiplayAlertIssue/DisplayAlertIssue/App.xaml.cs
+++ src/DiplayAlertIssue/DisplayAlertIssue/App.xaml.cs
@@ -9,6 +9,7 @@ public partial class App : Application
 		MainPage = new NavigationPage(new MainPage());
 	}
 
+    /**
     protected override void OnStart()
     {
         base.OnStart();
@@ -25,6 +26,39 @@ public partial class App : Application
 
         //Application.Current.MainPage.DisplayAlert("Alert Title", "The message!", "OK");
 
+    }
+    */
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+        window.Activated += Window_Activated;
+
+        return window;
+    }
+
+    static bool first_run = true;
+    private void Window_Activated(object sender, EventArgs e)
+    {
+        if (first_run)
+        {
+            first_run = false;
+            //displayAlert();
+        }
+    }
+
+    void displayAlert()
+    {
+        string message = """
+                    System.TypeInitializationException: The type initializer for 'Microsoft.AppCenter.Utils.ApplicationLifecycleHelperDesktop' threw an exception.
+                     ---> System.IO.FileNotFoundException: Could not load file or assembly 'System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089' or one of its dependencies.
+                    File name: 'System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089'
+                       --- End of inner exception stack trace ---
+                       at Microsoft.AppCenter.ApplicationLifecycleHelper.get_Instance()
+                    """;
+
+        Application.Current.MainPage.DisplayAlert("Alert Title", message, "OK");
     }
 }
 
--- src/DiplayAlertIssue/DisplayAlertIssue/MainPage.xaml.cs
+++ src/DiplayAlertIssue/DisplayAlertIssue/MainPage.xaml.cs
@@ -19,7 +19,17 @@ public partial class MainPage : ContentPage
 			CounterBtn.Text = $"Clicked {count} times";
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+
+        string message = """
+                    System.TypeInitializationException: The type initializer for 'Microsoft.AppCenter.Utils.ApplicationLifecycleHelperDesktop' threw an exception.
+                     ---> System.IO.FileNotFoundException: Could not load file or assembly 'System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089' or one of its dependencies.
+                    File name: 'System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089'
+                       --- End of inner exception stack trace ---
+                       at Microsoft.AppCenter.ApplicationLifecycleHelper.get_Instance()
+                    """;
+
+        Application.Current.MainPage.DisplayAlert("Alert Title", message, "OK");
+    }
 }
 
 
