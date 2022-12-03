236d7090dc7232650ef996db2bd0d919c7f06e8a
Yiannis Bourkelis <yiannis1234@gmail.com>
2022-12-03 14:30:49 +0200

    secure storage issue added

---
+++ src/SecureStorageMacCatalystIssue/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SecureStorageMacCatalystIssue;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ src/SecureStorageMacCatalystIssue/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace SecureStorageMacCatalystIssue;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	async private void OnCounterClicked(object sender, EventArgs e)
+	{
+		try
+		{
+			await SecureStorage.SetAsync("key", DateTime.Now.ToLongTimeString());
+
+			var result = await SecureStorage.GetAsync("key");
+            Console.WriteLine($"OK! Key value saved: {result}");
+        }
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex);
+			await DisplayAlert("Exception", ex.Message, "OK");
+		}
+    }
+}
+
+
