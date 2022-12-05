b9896965a83bd310b0894c9948fdd8c23100875c
Yiannis Bourkelis <yiannis1234@gmail.com>
2022-12-05 19:12:12 +0200

    CornerRadiusIssue

---
+++ src/CornerRadiusIssue/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CornerRadiusIssue;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
+
---
+++ src/CornerRadiusIssue/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CornerRadiusIssue;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
