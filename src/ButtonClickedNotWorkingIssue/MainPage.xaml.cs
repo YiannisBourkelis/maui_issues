5e63991b78fdc8ed4bcbec9fcff65f65a7cd0c19
Yiannis Bourkelis <yiannis1234@gmail.com>
2022-12-07 15:16:33 +0200

    ButtonNotClicked issue init commit

---
+++ src/ButtonClickedNotWorkingIssue/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace ButtonClickedNotWorkingIssue;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+    void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+        Navigation.PushAsync(new MainPage());
+    }
+}
+
+
